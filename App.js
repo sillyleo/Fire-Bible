5dc9b7b0c53208cbea4c29d87630fe34b5ae115f
Leo <leolin@me.com>
2018-09-03 23:25:24 +0800

    todo app

--- App.js
+++ App.js
@@ -7,14 +7,11 @@ import firebase from 'react-native-firebase';
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      // firebase things?
-    };
   }
 
   componentDidMount() {
-    // firebase things?
   }
+
 
   render() {
     return (
@@ -23,39 +20,3 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  logo: {
-    height: 120,
-    marginBottom: 16,
-    marginTop: 32,
-    width: 120,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  modules: {
-    margin: 20,
-  },
-  modulesHeader: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  module: {
-    fontSize: 14,
-    marginTop: 4,
-    textAlign: 'center',
-  }
-});
--- src/Todos.js
+++ src/Todos.js
@@ -1,40 +1,9 @@
 import React from 'react';
-import { SafeAreaView,ScrollView, View, Text, TextInput, Button } from 'react-native';
-import firebase from 'react-native-firebase';
 
-export default class Todos extends React.Component {
-  constructor() {
-    super();
-    this.ref = firebase.firestore().collection('todos');
-    this.state = {
-        textInput: '',
-    };
-  }
-
-  updateTextInput(value) {
-    this.setState({ textInput: value });
-    console.log(this.state)
-  }
-  
-  render() {
-    return (
-      <SafeAreaView>
-        <ScrollView>
-          <Text>List of TODOs</Text>
-        </ScrollView>
-        
-        <TextInput
-          placeholder={'Add TODO'}
-          value={this.state.textInput}
-          onChangeText={(text) => this.updateTextInput(text)}
-        />
-
-        <Button
-          title={'Add TODO'}
-          disabled={!this.state.textInput.length}
-          onPress={() => {}}
-          />
-      </SafeAreaView>
-    );
+class Todos extends React.Component {
+  render () {
+    return null;
   }
 }
+
+export default Todos;
